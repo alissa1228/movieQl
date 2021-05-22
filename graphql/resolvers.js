390cc384b311cae546de72e209a7de00ee64abd0
mac1228 <68986081+alissa1228@users.noreply.github.com>
2021-05-22 23:45:47 +0900

    nodemon 서버 충돌 오류

---
+++ graphql/resolvers.js
@@ -0,0 +1,9 @@
+const resolvers = {
+    Query: {
+      name: () => "croco"
+    }
+  };
+  
+export default resolvers;
+
+//해당 파일 export 오류로 index.ts에 직접 넣어줬음
--- index.ts
+++ index.ts
@@ -1,17 +1,19 @@
 import { GraphQLServer } from 'graphql-yoga';
-console.log('test');
+//import resolvers from './graphql/resolvers';
 
-// const typeDefs = {
-
-// }
-
-// const resolvers= {
-
-// }
+const resolvers = {
+    Query: {
+      name: () => "croco"
+    }
+  };
+  
 
 //server라는 새로운 변수를 만들고 여기에 새로운 graphQLServer를 만들어서 환경 설정을 해주면 된다.
 const server = new GraphQLServer({
-    //No schema defined-> 아직 정의 안해줬으니까! 오류 나는 건 당연한거임 
+    //모든 타입들에 대한 정의
+   typeDefs: "graphql/schema.graphql",
+   resolvers
 });
+
 
 server.start(()=>{console.log('server start!')})
