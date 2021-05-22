90b4d162baf4dd1c9969c2c4d5ee8115919ab20d
mac1228 <68986081+alissa1228@users.noreply.github.com>
2021-05-23 00:51:13 +0900

    nodemon 포트 충돌 해결

--- index.ts
+++ index.ts
@@ -1,19 +1,18 @@
 import { GraphQLServer } from 'graphql-yoga';
 //import resolvers from './graphql/resolvers';
 
+
 const resolvers = {
     Query: {
       name: () => "croco"
     }
   };
   
-
 //server라는 새로운 변수를 만들고 여기에 새로운 graphQLServer를 만들어서 환경 설정을 해주면 된다.
 const server = new GraphQLServer({
     //모든 타입들에 대한 정의
    typeDefs: "graphql/schema.graphql",
    resolvers
 });
-
 
 server.start(()=>{console.log('server start!')})
