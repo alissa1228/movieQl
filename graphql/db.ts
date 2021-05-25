f2601c5149e4edb62ac9b84c14745dc93de41261
mac1228 <68986081+alissa1228@users.noreply.github.com>
2021-05-25 20:52:19 +0900

    modify api url

--- graphql/db.ts
+++ graphql/db.ts
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch'
 ;//내 서버와 다른 URL을 통합하고 싶을 때의 예시.
-const API_URL = "https://api.themoviedb.org/3/movie/550?api_key=66ab4cea153083ca17a87321c592b83c";
+const API_URL = "https://yts.mx/api/v2/list_movies.json";
 
 export const getMovies = (limit, rating) => {
-  fetch(API_URL)
+  return fetch(`${API_URL}`)
   .then(res => res.json())
-  .then(json => console.log(json.data.movies));
+  .then(json=> console.log(json.data.movies));
 
 }
