7aa33868bb5e895b6686f0a708ad943b9570cc4b
mac1228 <68986081+alissa1228@users.noreply.github.com>
2021-05-25 21:09:08 +0900

    finishing first graphQL movie backend

--- graphql/db.ts
+++ graphql/db.ts
@@ -1,10 +1,16 @@
 import fetch from 'node-fetch'
 ;//내 서버와 다른 URL을 통합하고 싶을 때의 예시.
-const API_URL = "https://yts.mx/api/v2/list_movies.json";
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
 
 export const getMovies = (limit, rating) => {
-  return fetch(`${API_URL}`)
-  .then(res => res.json())
-  .then(json=> console.log(json.data.movies));
-
-}
+  let req_url = API_URL;
+  //limit가 0보다 크면 요청 url에 해당 string 추가.
+  if(limit>0) {
+    req_url += `limit=${limit}`
+  }
+  if(rating>0){
+    req_url += `&minimum_rating=${rating}`
+  }
+ return fetch(req_url).then(res=> res.json()).then(json=> json.data.movies);
+ }
+ 
