59347efe36e38253a1096a56784840a8e6363dbd
mac1228 <68986081+alissa1228@users.noreply.github.com>
2021-05-25 19:55:49 +0900

    add & delete movie

--- graphql/db.ts
+++ graphql/db.ts
@@ -24,11 +24,52 @@ export const people = [
       age: 27,
       gender: "male"
     },
-  ];
+];
+
+export let movies = [
+   {
+     id:1,
+     name:"LALA Land",
+     score : 4.5
+   },
+   {
+    id:2,
+    name:"Avengers - Age of Ultron",
+    score : 4
+  },
+  {
+    id:3,
+    name:"Wonder Woman",
+    score : 5
+  },
+]
 
   //id를 받아서 해당 id에 해당 하는 것만 걸러낸 후 리턴.
   //모든 people을 거친 다음 person.id값이 전달된 id 값과 같은 것만 리턴.
-  export const getById = id => {
+export const getById = id => {
     const filteredPeople = people.filter(person=> person.id === id);
     return filteredPeople[0];
+}
+
+export const deleteMovie = id => {
+    //같은 id를 가지지 않는 movie의 배열
+    const cleanedMovies = movies.filter(movie=> movie.id !== id);
+    //movies 길이가 같은 id를 가지지 않는 movie의 배열보다 길다면 delete, 그렇지 않으면 return false
+    if(movies.length > cleanedMovies.length){
+      movies = cleanedMovies
+      return true
   }
+  else{
+    return false;
+}
+}
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: movies.length + 1,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
+}
--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,4 +1,4 @@
-import { people, getById } from "./db";
+import { people, movies,getById, addMovie, deleteMovie } from "./db";
 
 // const resolvers = {
 //     Query: {
@@ -9,10 +9,16 @@ import { people, getById } from "./db";
 const resolvers = {
   Query: {
     people: () => people,
-    person: (_, {id}) => getById(id)
-  }
+    person: (_, {id}) => getById(id),
+    movies: () => movies,
+    movie: (_,{id}) => getById(id)
+  },
   //(_,) = (object, args) 
   //(현재 object를 보내는 object, args) -> playground에서 id:1을 요구하면 {id:1}이 찍힐 거임
   //{id} 는 args.id와 같음
+  Mutation:{
+    addMovie: (_,{name, score}) => addMovie(name, score),
+    deleteMovie: (_,{id}) => deleteMovie(id)
+  }
 };
 export default resolvers;
