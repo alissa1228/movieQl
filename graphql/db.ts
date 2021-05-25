d7f50bd5f04bad35e41dde5bc48411f3cca4555d
mac1228 <68986081+alissa1228@users.noreply.github.com>
2021-05-25 20:46:57 +0900

    0.12 fetch open api

--- graphql/db.ts
+++ graphql/db.ts
@@ -1,75 +1,10 @@
+import fetch from 'node-fetch'
+;//내 서버와 다른 URL을 통합하고 싶을 때의 예시.
+const API_URL = "https://api.themoviedb.org/3/movie/550?api_key=66ab4cea153083ca17a87321c592b83c";
 
-export const people = [
-    {
-    id : 1,
-    name : "crocodile",
-    age: 20,
-    gender: "female"
-    },
-    {
-      id : 2,
-      name : "johnny",
-      age: 28,
-      gender: "male"
-    },
-    {
-      id : 3,
-      name : "lucas",
-      age: 24,
-      gender: "male"
-     },
-    {
-      id : 4,
-      name : "tayong",
-      age: 27,
-      gender: "male"
-    },
-];
+export const getMovies = (limit, rating) => {
+  fetch(API_URL)
+  .then(res => res.json())
+  .then(json => console.log(json.data.movies));
 
-export let movies = [
-   {
-     id:1,
-     name:"LALA Land",
-     score : 4.5
-   },
-   {
-    id:2,
-    name:"Avengers - Age of Ultron",
-    score : 4
-  },
-  {
-    id:3,
-    name:"Wonder Woman",
-    score : 5
-  },
-]
-
-  //id를 받아서 해당 id에 해당 하는 것만 걸러낸 후 리턴.
-  //모든 people을 거친 다음 person.id값이 전달된 id 값과 같은 것만 리턴.
-export const getById = id => {
-    const filteredPeople = people.filter(person=> person.id === id);
-    return filteredPeople[0];
-}
-
-export const deleteMovie = id => {
-    //같은 id를 가지지 않는 movie의 배열
-    const cleanedMovies = movies.filter(movie=> movie.id !== id);
-    //movies 길이가 같은 id를 가지지 않는 movie의 배열보다 길다면 delete, 그렇지 않으면 return false
-    if(movies.length > cleanedMovies.length){
-      movies = cleanedMovies
-      return true
-  }
-  else{
-    return false;
-}
-}
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: movies.length + 1,
-    name,
-    score
-  };
-  movies.push(newMovie);
-  return newMovie;
 }
--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,24 +1,9 @@
-import { people, movies,getById, addMovie, deleteMovie } from "./db";
-
-// const resolvers = {
-//     Query: {
-//       name: () => "croco"
-//     }
-//   };
+import { getMovies } from "./db";
   
 const resolvers = {
   Query: {
-    people: () => people,
-    person: (_, {id}) => getById(id),
-    movies: () => movies,
-    movie: (_,{id}) => getById(id)
+    movies: () => getMovies
   },
-  //(_,) = (object, args) 
-  //(현재 object를 보내는 object, args) -> playground에서 id:1을 요구하면 {id:1}이 찍힐 거임
-  //{id} 는 args.id와 같음
-  Mutation:{
-    addMovie: (_,{name, score}) => addMovie(name, score),
-    deleteMovie: (_,{id}) => deleteMovie(id)
-  }
+
 };
 export default resolvers;
